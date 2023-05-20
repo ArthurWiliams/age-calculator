f8f18e1683cdaf3384e8c318d4f3d759b466cca8
Arthur Williams <arthurwiliams86@gmail.com>
2023-05-20 11:26:00 +0300

    Create field.ts

---
+++ src/field.ts
@@ -0,0 +1,61 @@
+import { IRule, TElement, Fields } from "./types";
+
+export default class Field {
+  element: TElement<HTMLInputElement>;
+  isValid: boolean = true;
+  rules: IRule[] = [];
+
+  // static isOneInvalid(fields: Field[]) {
+  //   return fields.some((field) => !field.isValid);
+  // }
+
+  constructor(field: HTMLInputElement | string) {
+    if (field instanceof HTMLInputElement) {
+      this.element = field;
+      return;
+    }
+
+    if (field.startsWith("#")) {
+      field = field.slice(1);
+    }
+
+    const FIELD = <TElement<HTMLInputElement>>document.getElementById(field);
+
+    if (FIELD === null) {
+      console.error(`Element with ${field} ID is not found!`);
+      return;
+    }
+
+    this.element = FIELD;
+  }
+
+  // addRule(message: string, validator: TValidator) {
+  //   this.rules.push({
+  //     message,
+  //     validator,
+  //   });
+  // }
+
+  validate(fields: Fields) {
+    const FIELD = this;
+
+    if (FIELD === null) {
+      this.isValid = true;
+      return null;
+    }
+
+    for (const rule of this.rules) {
+      if (!rule.validator(FIELD, fields)) {
+        this.isValid = false;
+        return rule.message;
+      }
+    }
+
+    this.isValid = true;
+    return null;
+  }
+
+  value() {
+    return this.element?.value ?? "";
+  }
+}
