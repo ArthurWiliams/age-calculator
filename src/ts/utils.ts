521f78d2c6c6ae6a9bc8fe38bda12a3df00593ca
Arthur Williams <arthurwiliams86@gmail.com>
2023-05-22 16:41:31 +0300

    Define a leap year validator

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -51,3 +51,7 @@ export function getElementById<T extends Element>(id: string): T {
 
   return ELEMENT;
 }
+
+export function isLeapYear(year: number): boolean {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}
