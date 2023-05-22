24d99882723bf2c9cdb03d78caa1cb1a6ccf7276
Arthur Williams <arthurwiliams86@gmail.com>
2023-05-22 17:28:33 +0300

    Define days of the months constant

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,6 +1,21 @@
 import Field from "./field";
 import { sub } from "date-fns";
 
+export const DAYS_OF_MONTHS = {
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+};
+
 export function showError(field: HTMLInputElement, message: string) {
   const ERROR_CONTAINER = field?.nextElementSibling;
 
